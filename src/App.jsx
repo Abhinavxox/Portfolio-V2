bbd52bfc3898d3d5ced711d7a9bfa87942d00f39
Abhinavxox <abhinavxox@gmail.com>
2023-05-13 15:05:48 +0530

    <Added>:[Code setup]

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <>
+      <div>
+        <h1 className="w-full text-center">PORTFOLIO</h1>
+      </div>
+    </>
+  );
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  mode: "jit",
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};

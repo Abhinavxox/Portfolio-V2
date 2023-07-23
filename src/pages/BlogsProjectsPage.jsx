5fae22d06bad1ee3a1154c9216045cda61bf5152
Abhinavxox <abhinavxox@gmail.com>
2023-07-23 15:11:15 +0530

    Fix : Search Bar and layout

--- src/pages/BlogsProjectsPage.jsx
+++ src/pages/BlogsProjectsPage.jsx
@@ -30,35 +30,37 @@ const BlogsProjectsPage = ({ content }) => {
     <div className="min-h-screen mb-10">
       <>
         <div className="form-control w-full p-5">
-          <div className="input-group">
-            <input
-              type="text"
-              placeholder="Search…"
-              className="input input-bordered w-full"
-              value={searchTerm}
-              onChange={handleSearch}
-            />
-            <button className="btn btn-square">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                />
-              </svg>
-            </button>
+          <div className="flex justify-center">
+            <div className="input-group w-full xl:w-3/4">
+              <input
+                type="text"
+                placeholder="Search…"
+                className="input input-bordered w-full"
+                value={searchTerm}
+                onChange={handleSearch}
+              />
+              <button className="btn btn-square">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                  />
+                </svg>
+              </button>
+            </div>
           </div>
         </div>
         {filteredData.length > 0 ? (
           <div className="">
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3">
               {filteredData.map((item) => (
                 <Card
                   key={item.title}
